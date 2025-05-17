610caf532002e9800ca52519e5a6d6a411035c79
Lse-Yeong <ocdee39@gmail.com>
2025-05-17 15:08:07 +0900

    섹션5 완료

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,7 +5,10 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 

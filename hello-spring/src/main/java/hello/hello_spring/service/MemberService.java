5cc1e8bc2619e60d4c668d62fb53767f72cb0ca1
Lse-Yeong <ocdee39@gmail.com>
2025-05-19 14:41:20 +0900

    섹션 8 수강 완료

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -25,7 +25,7 @@ public class MemberService {
     public Long join(Member member) {
         //같은 이름이 있는 중복 회원 X
         validateDuplicatedMember(member);
-        
+
         memberRepository.save(member);
         return member.getId();
     }
@@ -41,7 +41,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+            return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {

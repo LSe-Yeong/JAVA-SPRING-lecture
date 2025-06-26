4c31c1e3fb0acc6f40a8dbb6db925fce60acf330
Lse-Yeong <ocdee39@gmail.com>
2025-06-26 09:58:44 +0900

    섹션5 쿼리 메소드 수강 완료

--- datajpa/src/main/java/study/datajpa/entity/Member.java
+++ datajpa/src/main/java/study/datajpa/entity/Member.java
@@ -8,6 +8,10 @@ import lombok.*;
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id","username","age"})
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     public Member(String username) {
@@ -32,6 +36,11 @@ public class Member {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team team;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
 
     public void changeTeam(Team team) {
         this.team = team;

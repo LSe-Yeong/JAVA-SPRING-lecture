ca4d1ae33957bbcda3ec6eb114ebac52cba6b464
Lse-Yeong <ocdee39@gmail.com>
2025-06-26 11:03:48 +0900

    섹션 6 확장기능 수강 완료

---
+++ datajpa/src/main/java/study/datajpa/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity extends BaseTimeEntity{
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
--- datajpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ datajpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -261,4 +261,9 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findLockByUsername("member1");
 
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
